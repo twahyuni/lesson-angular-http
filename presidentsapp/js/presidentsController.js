b445b0f66845e5f11eac60e614a8d1f8e4136be0
Willie Tong <tongsinyin@gmail.com>
2016-03-21 09:46:39 +0800

    full crud solution

--- presidentsapp/js/presidentsController.js
+++ presidentsapp/js/presidentsController.js
@@ -3,9 +3,12 @@ var thePresidentsApp = angular.module('ThePresidentsApp');
 thePresidentsApp.controller('PresidentsController', ['$scope', '$http', function($scope, $http){
   $scope.presidents = [];
 
-  $scope.newPresident = {};
-  $scope.addPresident = addPresident;
+  $scope.president = {};
+  $scope.createMode = true;
+  $scope.newPresident = newPresident;
+  $scope.editPresident = editPresident;
   $scope.deletePresident = deletePresident;
+  $scope.savePresident = savePresident;
 
   getPresidents();
 
@@ -14,16 +17,23 @@ thePresidentsApp.controller('PresidentsController', ['$scope', '$http', function
       .get('http://localhost:3000/presidents')
       .then(function(response){
         $scope.presidents = response.data.presidents;
-    });
+      }
+    );
   }
 
-  function addPresident(){
+  function newPresident() {
+    $scope.president = {};
+    $scope.createMode = true;
+  }
+
+  function createPresident(){
     $http
-      .post('http://localhost:3000/presidents', $scope.newPresident)
+      .post('http://localhost:3000/presidents', $scope.president)
       .then(function(response){
         getPresidents();
-    });
-    $scope.newPresident = {};
+      }
+    );
+    $scope.president = {};
   }
 
   function deletePresident(president){
@@ -32,6 +42,35 @@ thePresidentsApp.controller('PresidentsController', ['$scope', '$http', function
       .then(function(response){
         var index = $scope.presidents.indexOf(president);
         $scope.presidents.splice(index, 1);
-      });
+      }
+    );
+  }
+
+  function editPresident(president) {
+    $scope.president = {
+      _id: president._id,
+      name: president.name,
+      start: president.start,
+      end: president.end
+    };
+    $scope.createMode = false;
+  }
+
+  function updatePresident() {
+    $http
+      .patch('http://localhost:3000/presidents/' + $scope.president._id, $scope.president)
+      .then(function(response){
+        getPresidents();
+      }
+    );
+    $scope.president = {};
+  }
+
+  function savePresident() {
+    if ($scope.createMode) {
+      createPresident();
+    } else {
+      updatePresident();
+    }
   }
 }]);
